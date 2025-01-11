aa9c0b6deef753b645ea38551eb1549080eae7b7
hissam <hissam@hissamnoMacBook-Pro.local>
2025-01-11 17:03:13 +0500

    kkk

--- src/redux/baseURL.js
+++ src/redux/baseURL.js
@@ -1,3 +1,3 @@
 
-// export const baseURL = "https://expedition-mangemnt.onrender.com";
-export const baseURL = "https://f25d-2407-d000-8-424a-892d-ebe5-4c71-d88d.ngrok-free.app"
+export const baseURL = "https://expedition-mangemnt.onrender.com";
+// export const baseURL = "https://f25d-2407-d000-8-424a-892d-ebe5-4c71-d88d.ngrok-free.app"
